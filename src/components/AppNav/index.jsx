54fdaff300bacdf9e0c0dcf472172185f0a99ff8
Ercilia Maria Coelho de Carvalho <erciliacarvalho2@gmail.com>
2025-07-01 18:53:05 -0300

    Ajustando componente Navegacao novo

--- src/components/AppNav/index.jsx
+++ src/components/AppNav/index.jsx
@@ -6,12 +6,10 @@ function AppNav(){
         <div className="bg-black flex flex-col md:flex-row justify-around items-center h-auto md:h-16 p-4 md:p-0 text-white">
             <h1 className="text-2xl flex items-center"> <AiOutlineLeftSquare /> Ercilia Carvalho</h1>
 
-            <div className="hidden md:block">
-                <Link to="/" className="bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full">Home</Link>
-                <Link to="/about" className="bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full">About Me</Link>
-                <Link to="/portfolio" className="bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full">Portfolio</Link>
-                <Link to="/skills" className="bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full">Skills</Link>
-            </div>
+                <Link to="/" className="hidden md:block bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full">Home</Link>
+                <Link to="/about" className="hidden md:block bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full">About Me</Link>
+                <Link to="/portfolio" className="hidden md:block bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full">Portfolio</Link>
+                <Link to="/skills" className="hidden md:block bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full">Skills</Link>
 
             <a href="#" className="bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full">
                 Hire Me
