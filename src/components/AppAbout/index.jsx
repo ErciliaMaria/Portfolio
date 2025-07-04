975c6317a7a89b92a936fa6ec6ece4ab7184c049
Ercilia Maria Coelho de Carvalho <erciliacarvalho2@gmail.com>
2025-07-04 16:03:27 -0300

    Ajustando componente AppAbout

--- src/components/AppAbout/index.jsx
+++ src/components/AppAbout/index.jsx
@@ -1,24 +1,38 @@
+import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
 import FotoPerfil from "../../assets/foto-perfil.png"
+import {Link} from "react-router-dom"
+
 function AppAbout(){
     return(
         <div className=" bg-black h-166 flex flex-col justify-center md:flex-row items-center gap-8 p-6">
+             <div className="text-white text-justify md:text-lg ">
+                <p className="text-1xl mb-5 max-w-lg text-slate-400" >I´m </p>
+
+                <h1><span className="text-4xl  hover:text-pink-700"> Ercilia </span></h1>
+                
+                <p className="text-xl max-w-lg  rounded-full my-8" >
+                    Currently studying Information System, focusing on improving my skills 
+                    in both Frontend and Backend.
+                    Here I will talk a little about myself and my latest.</p>
+            </div>
             <div>
-                <img className="w-88 h-88 md:w-94 md:h-94 rounded-full object-cover mask-r-from-10%" src={FotoPerfil}/>
+                <img className="w-70 h-64 md:w-64 md:h-94 rounded-full object-cover mask-r-from-10% border-4 border-pink-700" src={FotoPerfil}/>
             </div>
 
-            <div className="text-white max-w-xl text-justify md:text-lg space-y-6 container mx-auto">
-                <p className="text-xl h-auto max-w-lg  rounded-full" >Hi, my name is 
-                    <span className="underline text-2xl"> Ercilia Carvalho.</span></p>
-                <p className="text-xl h-auto max-w-lg  rounded-full my-8" >
-                    I am currently studying Analysis and Systems Development, focusing on improving my skills 
-                    in both Frontend and Backend.</p>
+            <div>
+                <h2 className='text-white text-3xl mb-2 mt-4 font-bold'>About Me</h2>
+                <p className=' text-white text-1xl max-w-lg mb-4'>
+                    I highlight my ability to learn quickly, my organization and my passion for solving problems with clean code.
+                </p>
 
-                 <p className="text-xl h-auto max-w-lg  rounded-full" >
-                    Here I will talk a little about myself and my latest <span className="underline
-                     hover:text-purple-600 text-4"> projects. </span></p>
-            
-                <p className="text-ml h-auto max-w-lg" >
-                    Ready?</p>
+                <Link to="/about" className="text-white bg-pink-700 px-5 py-2 hover:bg-pink-300 rounded-full mb-4">About Me</Link>
+
+                <div className=' text-pink-700 flex space-x-4 cursor-pointer mt-4'>
+                    <BsFacebook size={30} className=' border-pink-700 rounded-full cursor-pointer'/>
+                    <BsTwitter size={30} className=' border-pink-700 rounded-full cursor-pointer'/>
+                    <BsInstagram size={30} className=' border-pink-700 rounded-full cursor-pointer'/>
+                    <BsLinkedin size={30} className=' border-pink-700 rounded-full cursor-pointer'/>
+                </div>
             </div>
         </div>
     )
